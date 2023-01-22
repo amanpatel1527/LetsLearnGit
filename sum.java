2ab3625be39aaf4c68ab4985374f55c773d17dd5
Aman Patel <patelpatelaman00@gmail.com>
2023-01-22 10:25:01 +0530

    modified sum.java

--- sum.java
+++ sum.java
@@ -3,6 +3,7 @@ public class sum {
     public static void main(String[] args) {
         int a = 5;
         int b = 6;
+        System.out.print("The sum of two number is: ");
         System.out.println(a + b);
     }
 }
